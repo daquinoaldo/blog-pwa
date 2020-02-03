71608ac131ce592c1627ad36e4b0beaaa604cd07
daquinoaldo <daquino.aldo@gmail.com>
2020-02-03 19:24:30 +0100

    Set up WordPress theme

---
+++ index.php
@@ -0,0 +1,86 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <title><?php bloginfo('name'); ?></title><!-- update me--><!-- also in the manifest -->
+    <meta name="Description" content="<?php bloginfo('description'); ?>"><!-- update me--><!-- also in the manifest -->
+    <meta http-equiv=X-UA-Compatible content="IE=edge">
+    <meta name=viewport content="width=device-width, initial-scale=1">
+    <meta name="theme-color" content="#0288d1"><!-- update me--><!-- also in the manifest -->
+    <meta name="msapplication-TileColor" content="#0288d1"><!-- update me-->
+    <meta name="msapplication-TileImage" content="<?php bloginfo('template_url'); ?>/images/icons/msapplication-tile.png">
+    <meta name="mobile-web-app-capable" content="yes">
+    <meta name="apple-mobile-web-app-capable" content="yes">
+    <meta name="apple-mobile-web-app-status-bar-style" content="default">
+    <meta name="apple-mobile-web-app-title" content="<?php bloginfo('name'); ?>">
+    <meta name="application-name" content="<?php bloginfo('name'); ?>">
+    <link rel="apple-touch-icon" href="<?php bloginfo('template_url'); ?>/images/icons/apple-touch-icon.png">
+    <link rel="icon" type="image/png" href="<?php bloginfo('template_url'); ?>/images/icons/favicon-32x32.png" sizes="32x32">
+    <link rel="icon" type="image/png" href="<?php bloginfo('template_url'); ?>/images/icons/favicon-16x16.png" sizes="16x16">
+    <link rel="icon" type="image/x-icon" href="<?php bloginfo('template_url'); ?>/favicon.ico" />
+    <link rel="manifest" href="<?php bloginfo('template_url'); ?>/manifest.json">
+    <link rel="stylesheet" href="<?php bloginfo('template_url'); ?>/css/style.css">
+    <link rel="stylesheet" href="<?php bloginfo('template_url'); ?>/css/loading.css">
+  </head>
+  <body>
+    <nav>
+      <section id="arrow-back">
+        <a onclick="history.back()">
+          <img alt="back" src="<?php bloginfo('template_url'); ?>/images/nav/arrow-back.svg">
+        </a>
+      </section>
+      <section id="brand">
+        <a class="internal" href="/">
+          <img alt="logo" src="<?php bloginfo('template_url'); ?>/images/logo.svg">
+        </a>
+      </section>
+      <ul>
+        <li id="home-button">
+          <a class="internal" href="/">
+            <img alt="home" src="<?php bloginfo('template_url'); ?>/images/nav/home.svg">
+            <span>Home</span>
+          </a>
+        </li>
+        <li>
+          <a class="internal" href="/categories">
+            <img alt="categories" src="<?php bloginfo('template_url'); ?>/images/nav/categories.svg">
+            <span>Categories</span>
+          </a>
+        </li>
+        <li>
+          <a class="internal" href="/search">
+            <img alt="search" src="<?php bloginfo('template_url'); ?>/images/nav/search.svg">
+            <span>Search</span>
+          </a>
+        </li>
+        <li>
+          <a class="internal" href="/tags">
+            <img alt="tags" src="<?php bloginfo('template_url'); ?>/images/nav/tags.svg">
+            <span>Tags</span>
+          </a>
+        </li>
+        <li>
+          <a class="internal" href="/more">
+            <img alt="more" src="<?php bloginfo('template_url'); ?>/images/nav/more.svg">
+            <span>More</span>
+          </a>
+        </li>
+      </ul>
+    </nav>
+
+    <div id="loading"><div></div><div></div><div></div><div></div></div>
+
+    <main id="container">
+      <noscript>
+        This site requires JavaScript.
+        <a href="https://www.enable-javascript.com/" target="_blank">Here</a> you can find how to enable it in your browser.
+      </noscript>
+    </main>
+
+    <script src="<?php bloginfo('template_url'); ?>/js/content-provider.js"></script>
+    <script src="<?php bloginfo('template_url'); ?>/js/navigator.js"></script>
+    <script src="<?php bloginfo('template_url'); ?>/js/search.js"></script>
+    <script src="<?php bloginfo('template_url'); ?>/js/wordpress.js"></script>
+    <script src="<?php bloginfo('template_url'); ?>/js/install-sw.js"></script>
+  </body>
+</html>
