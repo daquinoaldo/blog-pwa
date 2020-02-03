41ef68889e719d8beaceb92c980d278e560bcb7f
daquinoaldo <daquino.aldo@gmail.com>
2020-02-03 19:06:39 +0100

    Improve Wordpress API url

--- js/wordpress.js
+++ js/wordpress.js
@@ -1,3 +1,5 @@
+const apiUrl = typeof API_URL !== 'undefined' ? API_URL : "/"
+
 /**
  * Make an HTTP GET request asynchronously.
  * @param url        Url of the request.
@@ -105,7 +107,7 @@ wp.getPosts = function(category = null, tag = null, postsNumber = -1,
   return new Promise(async resolve => {
     // calculate number of pages and posts in last page
     if (postsNumber === -1) {
-      url = API_URL + "posts/?per_page=1"
+      url = apiUrl + "?rest_route=/wp/v2/posts&per_page=1"
       if (category) url += "&categories=" + category
       if (tag) url += "&tags=" + tag
       await httpGetAsync(url, false).then(res => postsNumber = res.getResponseHeader("x-wp-total"))
@@ -119,7 +121,7 @@ wp.getPosts = function(category = null, tag = null, postsNumber = -1,
       // if only one page, query exactly that amount of posts
       perPage = pages > 1 ? wp.MAX_POST_PER_PAGE : postsNumber
       // prepare the url
-      let url = API_URL + "posts/?per_page=" + perPage + "&page=" + page
+      let url = apiUrl + "?rest_route=/wp/v2/posts&per_page=" + perPage + "&page=" + page
       if (category) url += "&categories=" + category
       if (tag) url += "&tags=" + tag
       // queue the job
@@ -139,7 +141,7 @@ wp.getPosts = function(category = null, tag = null, postsNumber = -1,
  * @returns {Promise<Object>} Promise resolved with the post.
  */
 wp.getPost = function(slug, fields = wp.POST_FIELDS_DEFAULT) {
-  return httpGetAsync(API_URL + "posts?slug=" + slug)
+  return httpGetAsync(apiUrl + "?rest_route=/wp/v2/posts&slug=" + slug)
       .then(post => wp.filter(post[0], fields))
 }
 
@@ -150,7 +152,7 @@ wp.getPost = function(slug, fields = wp.POST_FIELDS_DEFAULT) {
  * @returns {Promise<Array<Object>>} Promise resolved with the categories list.
  */
 wp.getCategories = function(orderBy = wp.CATEGORY_ORDERBY_DEFAULT, fields = wp.CATEGORY_FIELDS_DEFAULT) {
-  return httpGetAsync(API_URL + "categories")
+  return httpGetAsync(apiUrl + "?rest_route=/wp/v2/categories")
     .then(categories => wp.order(wp.filterAll(categories, fields), orderBy))
 }
 
@@ -161,6 +163,6 @@ wp.getCategories = function(orderBy = wp.CATEGORY_ORDERBY_DEFAULT, fields = wp.C
  * @returns {Promise<Array<Object>>} Promise resolved with the tags list.
  */
 wp.getTags = function(orderBy = wp.CATEGORY_ORDERBY_DEFAULT, fields = wp.FIELDS_ALL) {
-  return httpGetAsync(API_URL + "tags")
+  return httpGetAsync(apiUrl + "?rest_route=/wp/v2/tags")
     .then(tags => wp.order(wp.filterAll(tags, fields), orderBy))
 }
