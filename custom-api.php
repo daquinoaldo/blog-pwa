5006893db1463fe7b3c63e433f7b158f4dd6e918
daquinoaldo <daquino.aldo@gmail.com>
2020-05-10 15:38:51 +0200

    Add last_edit_date

--- custom-api.php
+++ custom-api.php
@@ -1,5 +1,16 @@
 <?php
 
+// Set the last edit date to the installation date
+add_option("last_edit_date", current_time("mysql"));
+
+// Update the last edit dat every time a post is added, edited or deleted
+function update_last_edit_date() {
+  update_option("last_edit_date", current_time("mysql"));
+}
+add_action('post_updated', 'update_last_edit_date');
+add_action('delete_post', 'update_last_edit_date');
+
+// ROUTE /posts
 function get_all_posts() {
   // get the posts
   $posts = get_posts(array(
@@ -20,11 +31,20 @@ function get_all_posts() {
   return $posts;
 }
 
+// ROUTE /last_edit_date
+function get_last_edit_date() {
+  return get_option("last_edit_date", "unknown");
+}
 
+// Register routes
 add_action("rest_api_init", function() {
   register_rest_route("simple-pwa/v1", "/posts", array(
     "methods" => "GET",
     "callback" => "get_all_posts"
+  ));
+  register_rest_route("simple-pwa/v1", "/last-edit-date", array(
+    "methods" => "GET",
+    "callback" => "get_last_edit_date"
   ));
 });
 
