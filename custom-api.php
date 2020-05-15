6841dbd056d57fe6cef9a51b99ae9d688077b7d3
Aldo D'Aquino <daquino.aldo@gmail.com>
2020-05-15 19:57:38 +0200

    Add API for categories and tags

--- custom-api.php
+++ custom-api.php
@@ -1,14 +1,26 @@
 <?php
 
 // Set the last edit date to the installation date
-add_option("last_edit_date", current_time("mysql"));
+add_option("last_edit", current_time("timestamp"));
 
 // Update the last edit dat every time a post is added, edited or deleted
-function update_last_edit_date() {
-  update_option("last_edit_date", current_time("mysql"));
+function update_last_edit() {
+  update_option("last_edit", current_time("timestamp"));
 }
-add_action('post_updated', 'update_last_edit_date');
-add_action('delete_post', 'update_last_edit_date');
+add_action('post_updated', 'update_last_edit');
+add_action('delete_post', 'update_last_edit');
+
+
+// ROUTE /last_edit
+function get_last_edit() {
+  return get_option("last_edit", "unknown");
+}
+
+function map_to_slug($list) {
+  $list = array_map(function ($item) { return $item->slug; }, $list);
+  if (is_null($list)) return [];
+  else return $list;
+}
 
 // ROUTE /posts
 function get_all_posts() {
@@ -24,27 +36,66 @@ function get_all_posts() {
     return (object) [
       "slug" => $post->post_name,
       "title" => $post->post_title,
-      "content" => $post->post_content
+      "content" => $post->post_content,
+      "categories" => map_to_slug(get_the_category($post->ID)),
+      "tags" => map_to_slug(get_the_tags($post->ID))
     ];
   }, $posts);
 
   return $posts;
 }
 
-// ROUTE /last_edit_date
-function get_last_edit_date() {
-  return get_option("last_edit_date", "unknown");
+// ROUTE /categories
+function get_all_categories() {
+  // get the categories
+  $categories = get_categories();
+
+  // remove unused fields
+  $categories = array_map(function ($category) {
+    return (object) [
+      "slug" => $category->slug,
+      "name" => $category->name
+    ];
+  }, $categories);
+
+  return $categories;
 }
+
+// ROUTE /tags
+function get_all_tags() {
+  // get the tags
+  $tags = get_tags();
+
+  // remove unused fields
+  $tags = array_map(function ($category) {
+    return (object) [
+      "slug" => $category->slug,
+      "name" => $category->name
+    ];
+  }, $tags);
+
+  return $tags;
+}
+
 
 // Register routes
 add_action("rest_api_init", function() {
+  register_rest_route("simple-pwa/v1", "/last-edit", array(
+    "methods" => "GET",
+    "callback" => "get_last_edit"
+  ));
   register_rest_route("simple-pwa/v1", "/posts", array(
     "methods" => "GET",
     "callback" => "get_all_posts"
   ));
-  register_rest_route("simple-pwa/v1", "/last-edit-date", array(
+  register_rest_route("simple-pwa/v1", "/categories", array(
     "methods" => "GET",
-    "callback" => "get_last_edit_date"
+    "callback" => "get_all_categories"
+  ));
+
+  register_rest_route("simple-pwa/v1", "/tags", array(
+    "methods" => "GET",
+    "callback" => "get_all_tags"
   ));
 });
 
